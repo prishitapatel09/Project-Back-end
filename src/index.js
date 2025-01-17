1f8a53ae6e6329138c1366c0095043a019ce2528
prishitapatel09 <prishitapatel09@gmail.com>
2025-01-16 20:58:28 -0500

    Handled utils(Custom API response & error handling)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : $
+            {process.env.PORT}`);
+    })
+
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+
+})
 
 /*
 import express from "express"
