6bd473294452724450f94c4c745ee2386dd388b8
Prishita Patel <124763279+prishitapatel09@users.noreply.github.com>
2025-01-20 16:51:55 -0500

    Added user and video model

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,42 +1,45 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate";
 
-const userSchema = new Schema (
+const videoSchema = new Schema (
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            index: true //give index to the most searchable feild of the database.
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true
-        },
-        fullname: {
-            type: String,
-            required: true,
-            trim: true,
-            index: true
-        },
-        avatar: {
+        videoFile:{
             type: String, //cloudinary url
-            required: true,
+            required: true
         },
-        coverImage:{
-            type: String, // cloudinary url
+        thumbnail:{
+            type: String, //cloudinary url
+            required: true
         },
-        watchHistory: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ]
+        title:{
+            type: String, 
+            required: true
+        },
+        description:{
+            type: String, 
+            required: true
+        },
+        duration:{
+            type: Number,
+            required: true
+        },
+        views:{
+            type: Number,
+            default: 0
+        },
+        isPublished:{
+            type: Boolean,
+            default: true
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },
+    {
+        timestamps: true
     }
 )
+videoSchema.plugin(mongooseAggregatePaginate)
 
-export const User = mongoose.model("User", userSchema)
+export const Video = mongoose.model("Video", videoSchema) 
