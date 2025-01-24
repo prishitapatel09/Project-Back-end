767db9033f0a7ba380e8437b3cfe9c47f7ab68dd
Prishita Patel <124763279+prishitapatel09@users.noreply.github.com>
2025-01-23 22:21:29 -0500

    All setup configured

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cros({
+app.use(
+  cros({
     origin: process.env.CROS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+)
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({expected: true, limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ expected: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
-export {app}
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
+export { app }
---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/register").post(registerUser)
+
+  
+
+
+export default router;
